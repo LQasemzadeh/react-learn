d901a98057b18b91747670185e2807c6884b8c5c
lqasemzadeh <lqasemzadeh@gmail.com>
2023-10-15 10:11:03 +0330

    Footer

--- src/components/ui/Footer.component.jsx
+++ src/components/ui/Footer.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 const FooterComponent = () => {
     return (
-        <div>
-            <h4>FooterComponent</h4>
-        </div>
+       <footer className='footer'>
+          <span>@CryptoCurrency.</span>
+           <span className='d-none d-md-inline'>All rights reserved</span>
+       </footer>
     );
 };
 
