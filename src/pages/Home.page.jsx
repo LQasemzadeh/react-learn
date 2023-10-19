96802e9881982a7eb20f3b17f4bc19be62fef240
lqasemzadeh <lqasemzadeh@gmail.com>
2023-10-19 11:26:51 +0330

    Home Page

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SpinnerComponent from "../components/ui/Spinner.component";
+import axios from "axios";
+import {baseUrl} from "../constants/api";
 
 const HomePage = () => {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+       setLoading(true);
+       axios(baseUrl)
+           .then((response) =>{
+               setLoading(false);
+               console.log(response.data);
+           })
+           .catch(error => {
+               setError("An error occured");
+           })
+    }, []);
+
     return (
-        <div>
-            <h1>HomePage</h1>
-            <SpinnerComponent />
+        <div className="container">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>Crypto Tracking</h1>
+                    <h4>A place to manage all of your trade information and action</h4>
+                    <h4>Do not forget to always using asset and risk management!!!!!</h4>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-12">
+                    {loading ? <SpinnerComponent /> : error !== "" ? <h1>{error}</h1> : <h1>Data Ready</h1>}
+                </div>
+            </div>
+
+
+
+
+
         </div>
     );
 };
